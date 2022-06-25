55e0fa6b18e24d2876f9dbab0f714bcf800a3b4d
Murat Yildirim <murat@lemonbrain.ch>
2022-06-25 22:06:58 +0200

    Game and Window classes added

---
+++ Game.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "Window.hpp"
+
+class Game {
+    public:
+        Game();
+        ~Game();
+
+        void HandleInput();
+        void Update();
+        void Render();
+
+        Window* GetWindow();
+
+        sf::Time GetElapsed();
+        void RestartClock();
+
+    private:
+        void MoveMushroom();
+
+        Window m_window;
+
+        sf::Clock m_clock;
+        sf::Time m_elapsed;
+
+        sf::Texture m_mushroomTexture;
+        sf::Sprite m_mushroom;
+        sf::Vector2i m_increment;
+};
--- main.cpp
+++ main.cpp
@@ -1,49 +1,14 @@
-#include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main()
 {
-    sf::RenderWindow 
-        window(sf::VideoMode(640, 480), "Bounchin mushroom");
-
-    sf::Texture mushroomTexture;
-
-    mushroomTexture.loadFromFile("mushroom.png");
-
-    sf::Sprite mushroom(mushroomTexture);
-    sf::Vector2u size =
-        mushroomTexture.getSize();
-    mushroom.setOrigin(size.x/2, size.y/2);
-    sf::Vector2f increment(0.4f, 0.4f);
-
-    while(window.isOpen())
-    {
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        if((mushroom.getPosition().x + (size.x / 2) >
-        window.getSize().x && increment.x > 0) ||
-        (mushroom.getPosition().x - (size.x / 2) < 0 &&
-        increment.x < 0))
-        {
-            // Reverse the direction on X axis.
-            increment.x = -increment.x;
-        }
-        if((mushroom.getPosition().y + (size.y / 2) >
-        window.getSize().y && increment.y > 0) ||
-        (mushroom.getPosition().y - (size.y / 2) < 0 &&
-        increment.y < 0))
-        {
-            // Reverse the direction on Y axis.
-            increment.y = -increment.y;
-        }
-        mushroom.setPosition(mushroom.getPosition() + increment);
-        mushroom.setColor(sf::Color(255, 0, 0, 255));
-        window.clear(sf::Color(16,16,16,255));
-        window.draw(mushroom); // Render our shape.
-        window.display();
+    Game game;
+    while (!game.GetWindow()->IsDone()) {
+        game.HandleInput();
+        game.Update();
+        game.Render();
+        // sf::sleep(sf::seconds(0.2));
+        game.RestartClock();
     }
 
     return 0;
